75e509db4bb939156ac695517fd1e4f681face15
Phil Splettstoeszer <stoeszer09@gmail.com>
2022-04-09 13:59:46 -0500

    adding color blocks and form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
 
 function App() {
+  let [colors, setColors] = useState([
+    'violet', 'blue', 
+    'lightblue', 'green', 
+    'greenyellow', 'yellow', 
+    'orange', 'red'
+  ])
+
+  let colorMap = colors.map((color, index) => {
+    return(
+      <ColorBlock color={color} key={index} />
+    )
+  })
+
+  const addColor = (newColor) => {
+    setColors([... colors, newColor])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {colorMap}
+      <ColorForm addColor={addColor} />
     </div>
   );
 }
